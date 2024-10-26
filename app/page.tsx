cb13ee4105543ccdec4bb4dda5d5c63bb32adabb
Gunjan Kawale <gunjankawale7@gmail.com>
2024-10-26 11:18:37 +0530

    Update tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Gunjan Kawale
       </h1>
       <p className="mb-4">
         {`As an aspiring Backend Developer specializing in Java, I am proficient in backend and database technologies. Known for my meticulous attention to detail and ability to adapt to new technologies, I focus on creating efficient and scalable solutions. Leveraging my robust problem-solving abilities and keen interest in cloud technologies and industry advancements, I am enthusiastic about contributing to a collaborative team atmosphere and evolving as a skilled backend professional.`}
       </p>
+      <i class="fa fa-github" aria-hidden="true"></i>
+      <i class="fa fa-linkedin" aria-hidden="true"></i>
+      <i class="fa fa-leetcode" aria-hidden="true"></i>
       <div className="my-8">
         <BlogPosts />
       </div>
